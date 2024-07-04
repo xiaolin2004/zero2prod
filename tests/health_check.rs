d89c4e7b744e15768461412b22437ecce46546f1
xiaolin2004 <1553367438@qq.com>
2024-07-05 01:42:28 +0800

    针对subscribe接口，定义了post方法的数据传输格式 使用框架的FormData，自动检查请求体中的数据格式是否正确，并自动返回错误码 新增数据库创建脚本，使用docker部署PostgreSQL

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,4 +1,5 @@
 use std::net::TcpListener;
+
 use zero2prod;
 
 #[tokio::test]
@@ -22,4 +23,39 @@ fn spawn_app() -> String {
 
     let _ = tokio::spawn(server);
     format!("http://127.0.0.1:{}", port)
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+    let response = client
+        .post(&format!("{}/subscription", &app_address))
+        .header("Content-Type", "application/x-www-form-unlencoded")
+        .body(body)
+        .send()
+        .await.expect("Fail to execute request.");
+    assert_eq!(200, response.status().as_u16());
+}
+
+#[tokio::test]
+async fn subscribe_return_a_415_when_data_is_missing() {
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+    // table-driven-test
+    let test_cases = vec![
+        ("name=le%20guin", "missing the email"),
+        ("email=ursula_le_guin%40gmail.com", "missing the name"),
+        ("", "missing both name and email"),
+    ];
+    for (invalid_body, error_message) in test_cases {
+        let response = client
+            .post(&format!("{}/subscription", app_address))
+            .header("Content-Type", "application/x-www-form-unlencoded")
+            .body(invalid_body)
+            .send().await.expect("Fail to execute request.");
+        assert_eq!(
+            415, response.status().as_u16(), "The API did not fail with 400 Bad Request when the payload was {}.", error_message);
+    }
 }
